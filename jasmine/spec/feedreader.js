2b968a31ea0d4a41ff918dd8f56fa47d5a3b32e9
Matt Peters <matthew.george.peters@gmail.com>
2018-08-12 14:47:59 -0400

    loops through allFeeds and checks url and name

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,12 +31,24 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('URLs are defined', function(){
+            allFeeds.forEach(function(element){
+                expect(element.url).toBeDefined();
+                expect(element.length).not.toBe(0);
+            })
+        });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('names are defined', function(){
+            allFeeds.forEach(function(element){
+                expect(element.name).toBeDefined();
+                expect(element.length).not.toBe(0);
+            })
+        });
     });
 
 
